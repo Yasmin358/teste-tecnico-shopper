cd94c5271f1c2f8071c37a3d5d80be39166cd9d0
Yasmin Amorin <amorin.yasmin378@gmail.com>
2024-11-24 15:58:26 -0300

    Revert "feat: RequestRide"

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Form from "./components/form.tsx";
 
 function App() {
-
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const callBackendAPI = async () => {
+      try {
+        const response = await fetch("/api");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const body = await response.json();
+        setData(body.message);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    callBackendAPI();
+  }, []);
   return (
     <div className="App">
-      <Form />
+      <header className="App-header">
+        <img className="App-logo" src={logo} alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <p style={{ color: "white" }}>{data}</p>
+      </header>
     </div>
   );
 }
