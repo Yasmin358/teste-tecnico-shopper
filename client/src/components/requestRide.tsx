f9542d9f6c5cd765d091894808e4883e9fb4947a
Yasmin358 <amorin.yasmin378@gmail.com>
2024-11-23 22:18:19 -0300

    feat: RequestRide

---
+++ client/src/components/requestRide.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Form from './form.tsx';
+
+export default function RequestRide() {
+    return (
+        <main>
+            <h1>Solicitar Viagem</h1>
+            <Form />
+        </main>
+    );
+}
